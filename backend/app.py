48b17cccaecd3e194af7a933524f2f0cacbd839e
Mondem Jagadeesh <jagadeeshmondem@gmail.com>
2026-09-17 16:43:08 +0530

    app modified

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "Calculator Backend is Running"
 
 
 @app.route("/calculate", methods=["POST"])
@@ -18,14 +20,18 @@ def calculate():
 
     if operator == "+":
         result = num1 + num2
+
     elif operator == "-":
         result = num1 - num2
+
     elif operator == "*":
         result = num1 * num2
+
     elif operator == "/":
         if num2 == 0:
             return jsonify({"error": "Cannot divide by zero"}), 400
         result = num1 / num2
+
     else:
         return jsonify({"error": "Invalid operator"}), 400
 
@@ -33,4 +39,4 @@ def calculate():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
